19827674c1282e5c57be1e9bfce14359f75e6a2a
Roberto Dedoro <mi.roberto.d@gmail.com>
2022-03-25 08:56:49 +0100

    use loop

--- macos-setup.sh
+++ macos-setup.sh
@@ -4,44 +4,16 @@ sudo spctl --master-disable
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 brew update
-brew install bash
-brew install git
-brew install automake
-brew install binutils
-brew install gcc
-brew install telnet
-brew install wget
-brew install vim
-brew install gnu-sed
-brew install htop
-brew install iproute2mac
-brew install jq
-brew install netcat
-brew install p7zip
-brew install pwgen
-brew install watch
-brew install tree
-brew install shellcheck
-brew install nmap
 
-brew cask install google-chrome
-brew cask install docker
-brew cask install discord
-brew cask install android-file-transfer
-brew cask install vlc
-brew cask install tableplus
-brew cask install kap
-brew cask install postman
-brew cask install tunnelblick
-brew cask install sensiblesidebuttons
-brew cask install mos
-brew cask install burp-suite
-brew cask install wireshark
-brew cask install virtualbox
-brew cask install rectangle
-brew cask install menumeters
-#brew cask install iterm2 # Natural text editing
-brew cask install visual-studio-code
-brew cask install mnemosyne
+leaves="./leaves.txt"
+while IFS= read -r l; do
+  brew install "$l"
+done <"$leaves"
+exit 0
+
+casks="./casks.txt"
+while IFS= read -r c; do
+  brew install "$c"
+done <"$casks"
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
